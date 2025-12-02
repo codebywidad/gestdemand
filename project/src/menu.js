d9170e5d337376c17aa5771abc38f5d9f8b1d481
3azzyy <adamsouhailbida@gmail.com>
2025-12-02 01:06:35 +0100

    update menu

--- project/src/menu.js
+++ project/src/menu.js
@@ -4,7 +4,7 @@ export default function Menu (){
     <nav class="barre-navigation">
             <div class="logo">&lt;Gest<span>Demand</span>/&gt;</div>
             <ul class="menu-navigation">
-                <li><a href="#fonctionnalites">Fonctionnalités</a></li>
+                <li><a href="#fonctionnalites">Fonctionnalit</a></li>
                 <li><a href="#technologies">Technologies</a></li>
                 <li><a href="#processus">Processus</a></li>
                 <li><a href="#contact">Contact</a></li>
